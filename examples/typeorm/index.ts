556ec0ccf360e18417d38c5b1e11fd1887e1f420
DillonSadofsky <dsadofsky@isoftdata.com>
2025-12-29 12:32:34 -0600

    Fixed import

--- examples/typeorm/index.ts
+++ examples/typeorm/index.ts
@@ -15,8 +15,8 @@ import { Company } from "./entities/Company.js";
 import { Desk } from "./entities/Desk.js";
 import { Employee } from "./entities/Employee.js";
 import { PersonalComputer } from "./entities/PersonalComputer.js";
-import typeormResolvers from "./resolvers.js";
 import { ApolloServerLoaderPlugin } from "index.js";
+import typeormResolvers from "../../examples/typeorm/resolvers/index.js";
 
 export function connect(logging: boolean = false) {
   return createConnection({
