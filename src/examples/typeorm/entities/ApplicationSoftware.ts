b6254c45e9b5048c4a95f67772f6253ee181e787
DillonSadofsky <dsadofsky@isoftdata.com>
2025-12-29 18:50:28 -0600

    Schema was complaining about null values in the ApplicationSoftware entity, so made fields nullable

--- src/examples/typeorm/entities/ApplicationSoftware.ts
+++ src/examples/typeorm/entities/ApplicationSoftware.ts
@@ -1,5 +1,5 @@
 import { Field, ObjectType } from "type-graphql";
-import { Entity, ManyToMany, ManyToOne, PrimaryColumn } from "typeorm";
+import { Column, Entity, ManyToMany, ManyToOne, PrimaryColumn } from "typeorm";
 import { type Lazy } from "../types/Lazy.js";
 import { Base, Company, PersonalComputer } from "./index.js";
 import { TypeormLoader } from "../../../decorators/typeorm/TypeormLoader.js";
@@ -9,14 +9,18 @@ import { TypeormLoader } from "../../../decorators/typeorm/TypeormLoader.js";
 export class ApplicationSoftware extends Base<ApplicationSoftware> {
   @Field()
   @PrimaryColumn()
+  id: number;
+
+  @Field()
+  @Column({ nullable: true })
   name: string;
 
   @Field()
-  @PrimaryColumn()
+  @Column({ nullable: true })
   majorVersion: number;
 
   @Field()
-  @PrimaryColumn()
+  @Column({ nullable: true })
   minorVersion: number;
 
   @Field((type) => [PersonalComputer])
